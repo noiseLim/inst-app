1aff69f8eced8d45e6b7b766a801db553d3ae657
noiseLim <noiselim@gmail.com>
2021-02-08 22:21:01 +0300

    added font, start design in post-list-item

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -5,16 +5,15 @@ import './post-list-item.scss';
 
 const PostListItem = ({postItem}) => {
 
-    const {title, url, category} = postItem;
+    const {title, user, url} = postItem;
 
     return (
         <li className="post__item">
             <Link to = {`/${postItem.id}`}>
-                <div className="post__title">{title}</div>
+                <div className="post__title">{user}</div>
                 <img className="post__img" src={url} alt={title}></img>
-                <div className="post__category">Category: <span>{category}</span></div>
+                <div className="post__category"><span>{user}</span> {title}</div>
                 <button className="post__btn">Add comment</button>
-                <span className = {`post__category_Img ${category}`}></span>
             </Link>      
         </li>
     )
